6ca5a8b291d3498deebe3b6c2dfe48cf9e02faf7
William Reilly <William.Reilly2010@gmail.com>
2015-02-16 22:01:59 -0500

    Development fix.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
-  
+  has_many :user_roles
+  has_many :roles, through: :user_roles
+
+  validates :email, uniqueness: true, presence: true
+  validates :name, presence: true
+
+  def user_must_have_at_least_one_role
+    errors.add(:role, ", at least one is required.") if self.roles.empty?
+  end
 end
